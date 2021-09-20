20a968647592a8512701d97cebcf8b26492f1cb3
Sam Chen <75424121@qq.com>
2021-09-20 10:00:33 +0800

    防抖、call函数、apply函数

--- functions/src/utils.js
+++ functions/src/utils.js
@@ -1,5 +1,5 @@
+// 节流
 function throttle(callback, wait) {
-  // 起始时间
   let start = 0
 
   return function (e) {
@@ -9,4 +9,37 @@ function throttle(callback, wait) {
       start = now
     }
   }
+}
+// 防抖
+function debounce(callback, wait) {
+  let timeId = null
+  return function (e) {
+    if (timeId !== null) {
+      clearTimeout(timeId)
+    }
+    timeId = setTimeout(() => {
+      callback.call(this, e)
+      timeId = null
+    }, wait)
+  }
+}
+// call函数封装
+function call(fn, obj, ...args) {
+  if (obj === undefined || obj === null) {
+    obj = globalThis // es11
+  }
+  obj.tempFn = fn
+  const result = obj.tempFn(...args)
+  delete obj.tempFn
+  return result
+}
+// apply函数封装
+function apply(fn, obj, args) {
+  if (obj === undefined || obj === null) {
+    obj = globalThis // es11
+  }
+  obj.tempFn = fn
+  const result = obj.tempFn(...args)
+  delete obj.tempFn
+  return result
 }
